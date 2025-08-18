4d0f30892532a482ca2c2787ed9e1b9f5141f7fe
sergiozuloaga10 <sergio-sst@hotmail.com>
2025-08-18 17:32:58 -0600

    Cambios

---
+++ arreglos/DeclaracionArreglos.java
@@ -0,0 +1,13 @@
+package arreglos;
+
+class DeclaracionArreglos{
+    public static void main(String[] args) {
+        //Declaracion de arreglos:
+        //Declaracion:    tipo[] nombreArreglo;
+        final int N = 10;
+        int[] numeros; //Declaracion
+        numeros = new int[N];
+        
+         
+    }
+}
