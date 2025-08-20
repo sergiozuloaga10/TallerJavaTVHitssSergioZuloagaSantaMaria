1119ad3cd0f5c998f1cc46ceafa8f5424d99da93
sergiozuloaga10 <sergio-sst@hotmail.com>
2025-08-20 17:34:31 -0600

    clase 20/08

---
+++ pooTV/poo/Automovil.java
@@ -0,0 +1,13 @@
+package pooTV.poo;
+
+public class Automovil {
+    public static void main(String[] args) {
+        //atributos     public, private, protected, (default)
+        String marca;
+        String color;
+        int anio;
+        Integer numerpPuertas = 4;
+
+        //metodos
+    }
+}
---
+++ pooTV/test/TestMain.java
@@ -0,0 +1,10 @@
+package pooTV.test;
+
+import pooTV.poo.Automovil;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Automovil automovil = new Automovil();
+        
+    }
+}
