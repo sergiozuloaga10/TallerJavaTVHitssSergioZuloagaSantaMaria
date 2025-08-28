482ac2bfb0a603ca09bfb5b2f770d2f90f8a480f
sergiozuloaga10 <sergio-sst@hotmail.com>
2025-08-28 17:01:45 -0600

    clase 28/08

---
+++ herencia/clases/Persona.java
@@ -0,0 +1,35 @@
+package clases;
+
+public class Persona{
+    private String name;
+    private int edad;
+
+    public Persona() {
+    }
+
+    public Persona(String name, int edad) {
+        this.name = name;
+        this.edad = edad;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void mostrarInfo(){
+        System.out.println();
+    }
+    
+}
